23286851cd068d8d7917071237b01e81ab91c142
StealthM0de <jedongfallorina@gmail.com>
2024-03-05 16:15:29 +0800

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,10 @@
-import { RotatingLines } from 'react-loader-spinner';
+import { FadeLoader } from 'react-spinners';
 import css from './Loader.module.css';
 
 export const Loader = () => {
   return (
     <div className={css.loaderWrap}>
-      <RotatingLines
-        visible={true}
-        height="96"
-        width="96"
-        color="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-      />
+      <FadeLoader color="#36d7b7" />
     </div>
   );
 };
